4c2d34ff1705f5eac13332ed69471177645f2dc0
dmb-2222 <dmb-2222@yandex.ru>
2020-05-18 12:46:43 +0300

    Updates

---
+++ precache-manifest.7e07be4fb983bec9e248f51d9bc1cd47.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de420083060cebfba3dadd7321645389",
+    "url": "/macpaw-chucknorris/index.html"
+  },
+  {
+    "revision": "29da2b93c003d4fce0e4",
+    "url": "/macpaw-chucknorris/static/css/main.e4a91b6a.chunk.css"
+  },
+  {
+    "revision": "94d336d927e483b68cf9",
+    "url": "/macpaw-chucknorris/static/js/2.79edff84.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/macpaw-chucknorris/static/js/2.79edff84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29da2b93c003d4fce0e4",
+    "url": "/macpaw-chucknorris/static/js/main.2055bf3a.chunk.js"
+  },
+  {
+    "revision": "e0b651f522a345ea0f0e",
+    "url": "/macpaw-chucknorris/static/js/runtime-main.f9074482.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "99682a78fa4fe61e1177b94757336bbf",
+    "url": "/macpaw-chucknorris/static/media/Roboto-BlackItalic.99682a78.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/macpaw-chucknorris/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/macpaw-chucknorris/static/media/Roboto-LightItalic.a3ce4440.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/macpaw-chucknorris/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/macpaw-chucknorris/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "35a9c89aff1396595ad345e378a32aca",
+    "url": "/macpaw-chucknorris/static/media/Roboto-ThinItalic.35a9c89a.ttf"
+  },
+  {
+    "revision": "0ebb74f0b6bf8b171c6b007c866481f7",
+    "url": "/macpaw-chucknorris/static/media/icon-heart-empty.0ebb74f0.svg"
+  },
+  {
+    "revision": "0865c8322b3cc6f1bed5a8f1f35f00b5",
+    "url": "/macpaw-chucknorris/static/media/icon-heart-full.0865c832.svg"
+  },
+  {
+    "revision": "b6bce73c2360f0c4afddbcf5f3b09aa4",
+    "url": "/macpaw-chucknorris/static/media/icon-letter-joke.b6bce73c.svg"
+  },
+  {
+    "revision": "358f75fed4bb54fe11cd68ab147b7c8d",
+    "url": "/macpaw-chucknorris/static/media/iconFavNoActive.358f75fe.svg"
+  },
+  {
+    "revision": "dbd9017e3a6d90935746162a2a72431f",
+    "url": "/macpaw-chucknorris/static/media/icone-fav-active.dbd9017e.svg"
+  },
+  {
+    "revision": "5a9fab2a2256d4fc96d72027aa339fb4",
+    "url": "/macpaw-chucknorris/static/media/icone-letter-favorite.5a9fab2a.svg"
+  }
+]);
